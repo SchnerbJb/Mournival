016dafa66cfead43c16219c6a7db3a82329ee1e7
Jean-Baptiste Schnerb <j.schnerb@gmail.com>
2026-04-08 20:46:54 +0200

    Implemented turned sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,69 @@
 extends CharacterBody2D
 
 var run_speed = 350
-var animation: AnimatedSprite2D
+var animated_sprite: AnimatedSprite2D
+
+enum PlayerDirection {FRONT, BACK, SIDE_LEFT, SIDE_RIGHT, BACK34LEFT, BACK34RIGHT, FRONT34LEFT, FRONT34RIGHT}
 
 func _ready() -> void:
-	animation = get_node("AnimatedSprite2D")
-	animation.visible = false
+	animated_sprite = get_node("PlayerSprite")
+
 
 func get_input() -> void:
-	look_at(get_global_mouse_position())
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * run_speed
+	var animation_direction: PlayerDirection
+	if input_direction.x > 0:
+		if input_direction.y > 0:
+			animation_direction = PlayerDirection.FRONT34RIGHT
+		elif input_direction.y < 0:
+			animation_direction = PlayerDirection.BACK34RIGHT
+		else:
+			animation_direction = PlayerDirection.SIDE_RIGHT
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("attack"):
-		attack()
+	elif input_direction.x < 0:
+		if input_direction.y > 0:
+			animation_direction = PlayerDirection.FRONT34LEFT
+		elif input_direction.y < 0:
+			animation_direction = PlayerDirection.BACK34LEFT
+		else:
+			animation_direction = PlayerDirection.SIDE_LEFT
+	
+	else:
+		if input_direction.y > 0:
+			animation_direction = PlayerDirection.FRONT
+		elif input_direction.y > 0:
+			animation_direction = PlayerDirection.BACK
 
-func attack() -> void:
-	animation.visible = true
-	animation.play()
+
+	animated_sprite.animation = get_animation_name(animation_direction)
+
+
+func get_animation_name(direction: PlayerDirection) -> StringName:
+	match (direction):
+		PlayerDirection.BACK:
+			return &"player_animation_b"
+		PlayerDirection.FRONT:
+			return &"player_animation_f"
+		PlayerDirection.BACK34LEFT:
+			return &"player_animation_b34l"
+		PlayerDirection.BACK34RIGHT:
+			return &"player_animation_b34r"
+		PlayerDirection.FRONT34LEFT:
+			return &"player_animation_f34l"
+		PlayerDirection.FRONT34RIGHT:
+			return &"player_animation_f34r"
+		PlayerDirection.SIDE_LEFT:
+			return &"player_animation_sl"
+		PlayerDirection.SIDE_RIGHT:
+			return &"player_animation_sr"
+		_:
+			return &"player_animation_f"
+
 
 func _physics_process(_delta) -> void:
 	get_input()
 	move_and_slide()
 
-func _on_animated_sprite_2d_animation_finished() -> void:
-	animation.visible = false
+func _process(_delta: float) -> void:
+	pass
